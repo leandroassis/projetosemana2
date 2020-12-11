517f3c0ecbcd32933ecab4a2c4756a91ca96b8c3
Leandro <assissantosleandro@gmail.com>
2020-12-11 15:09:39 -0300

    Feat:Adicionar novos post e contador ON

--- triangleapp/src/Pages/WritePost/index.js
+++ triangleapp/src/Pages/WritePost/index.js
@@ -3,12 +3,21 @@ import { StyleSheet,View, TouchableOpacity, Text, StatusBar, TextInput, Keyboard
 import { set } from 'react-native-reanimated';
 
 
-export default function WritePost({navigation,route}) {
+export default function WritePost({navigation, listLeght}) {
   const [counter,setCounter] = useState(0);
   function incrementOnType(){
     setCounter(counter+1);
   }
   const [postText, setPostText] = React.useState("");
+
+  function ReturnData(){
+    navigation.navigate("Feed",{
+     data:{
+       id:listLeght+1, 
+       message:postText,
+       send:"True"
+      }})
+  }
   return (
     <KeyboardAvoidingView style={styles.container} behavior={"padding"}>
       <View style={styles.container}>
@@ -17,14 +26,14 @@ export default function WritePost({navigation,route}) {
               <TouchableOpacity onPress={()=> navigation.navigate("Feed")} style={styles.headerPost} >
                   <Text style={styles.headerButtom}>Fechar</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => navigation.navigate("Feed",{message:"AloAlo"})} style={styles.headerPostRight} >
+              <TouchableOpacity onPress={ReturnData} style={styles.headerPostRight} >
                   <Text style={styles.headerButtom}>Enviar</Text>
               </TouchableOpacity>
           </View>
           <View style={styles.Text}>
               <Image source={require("../../Components/Images/gaia.jpeg")} style={styles.photo}/>
               <View style={{width:330}}>
-              <TextInput placeholder="Escreva algo legal para postar" maxLength={200} multiline={true} onChangeText={incrementOnType}  autoFocus={true} style={{width:"100%", margin:20, marginLeft:5, fontSize:20}} />
+              <TextInput placeholder="Escreva algo legal para postar" maxLength={200} multiline={true} onChangeText={incrementOnType} onChange={setPostText} autoFocus={true} style={{width:"100%", margin:20, marginLeft:5, fontSize:20}} />
               </View>
           </View>
           <View style={styles.footer}>
